3b6fb243346ce4687965806b6497ce1beb4944e3
Piotr Szaran <46755094+iPior@users.noreply.github.com>
2026-09-01 17:11:46 -0400

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const map = new Map();
+
+        for (let i = 0; i < strs.length; i++) {
+
+            const str = strs[i].split('').sort().join('');
+
+            if (map.has(str)){
+                const newArray = [...(map.get(str)), strs[i]]
+                map.set(str, newArray)
+            }
+            else {
+                map.set(str,[strs[i]])
+            }    
+        }
+
+        const output = []
+        map.forEach((value) => {
+            output.push(value);
+        });
+
+        console.log(output);
+        return output;
+
+    }
+}
