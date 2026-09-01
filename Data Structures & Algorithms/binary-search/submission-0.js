561899800442e57cfd742c37f34eb86f33af46a3
Piotr Szaran <46755094+iPior@users.noreply.github.com>
2026-09-01 08:17:05 -0400

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        var left = 0
+        var right = nums.length - 1
+
+        while (left <= right){
+            var mid = Math.floor(left + (right - left) / 2);
+            
+            if (nums[mid] === target) return mid;
+
+            if (nums[mid] > target) {
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }    
+        }
+    
+        return -1
+
+    }
+}
