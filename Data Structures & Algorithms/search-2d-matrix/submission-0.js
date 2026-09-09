1bfc2b942a1106896f4aafe1fc4e5d4021b1726e
Piotr Szaran <46755094+iPior@users.noreply.github.com>
2026-09-09 08:05:17 -0400

    Add: search-2d-matrix - submission-0

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-0.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+    searchMatrix(matrix, target) {
+        let arr = matrix.flat()
+        let left=0;
+        let right = arr.length-1
+
+        while(left<=right){
+            let mid = Math.floor((left+right)/2);
+
+            if(arr[mid]===target){
+                return true
+            }
+            else if (arr[mid] < target){
+                left = mid+1
+            }
+            else {
+                right = mid-1
+            }
+        }
+        return false
+    }
+}
